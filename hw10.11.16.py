37bb03fcb8fa599787f9d0a2b0f2a37790d20586
Oleg <o.v.gamov@gmail.com>
2016-11-15 20:20:20 +0200

    fix #23, add 25, 26, 27

--- hw10.11.16.py
+++ hw10.11.16.py
@@ -1,10 +1,10 @@
+import random
 # 16. Каждому символу в таблице символов Unicode соответствует число.
 # Написать функцию, которая расчитывает сумму чисел, которые соответствую символам,
 #  стоящим между двумя заданными.
-# Например, в функцию передаются символы x и z.
-# Значит надо вычислить сумму кодов символов x,y,z.
+# Например, в функцию передаются символы x и z. Значит надо вычислить сумму кодов символов x,y,z.
 
-first_s, last_s = "x", "z"
+first_s, last_s = "a", "c"
 
 def sum_of_unicode(first_s, last_s):
     num_f = ord(first_s)
@@ -29,8 +29,6 @@ def sum_num_of_pow(n):
         if i == n**pow:
             pow = pow + 1
             sum = sum + i
-        else:
-            pow = pow
     return sum
 result = sum_num_of_pow(n)
 print("The sum of the numbers which are powers of %d is: %d" % (n, result))
@@ -41,12 +39,10 @@ list = [11, 7, 13, 4]
 print("The average value of", (list))
 
 def aver_all_elem(list):
-    aver_el = list[0]
+    sum_el = 0
     for i in range(len(list)):
-        ce = list[i]
-        if ce < aver_el:
-            aver_el=aver_el+ce
-    return aver_el / len(list)
+        sum_el = sum_el + list[i]
+        return sum_el / len(list)
 
 res = aver_all_elem(list)
 print("is", res)
@@ -64,9 +60,78 @@ def dif_sum_ev_od(lst):
             even = even + ce
         else:
             odd = odd + ce
-        ce = ce // 10
     diff = even - odd
     return diff
 
 res = dif_sum_ev_od(lst)
 print("The difference sums of even and odd is: ", res)
+
+# 25. Создайте список на 50 элементов из всех нечётных чисел от 1 до 99, выведите его на экран
+# в строку, а затем этот же список выведите на экран тоже в строку, но в случайном порядке
+# (например, 99 11 43 19 … 7 91 3 1).
+
+list = [i for i in range (1,100, 2)]
+print(list)
+random.shuffle(list)
+print(list)
+
+# 26. Создайте 2 списка из 5 случайных целых чисел из отрезка [0;5] каждый, выведите списки на
+# экран в двух отдельных строках. Посчитайте среднее арифметическое элементов каждого
+# списка и сообщите, для какого из списков это значение оказалось больше (либо сообщите,
+# что их средние арифметические равны).
+
+list1 = []
+list2 = []
+def fill_list(list):
+    lst = [random.randint(0, 5) for i in range(5)]
+    return lst
+
+lst1, lst2 = fill_list(list1), fill_list(list2)
+print("List_1: ", lst1)
+print("List_2: ", lst2)
+
+def aver(lst):
+    sum = 0
+    for i in range(len(lst)):
+        sum = sum + lst[i]
+    avr = sum / len(lst)
+    return avr
+
+res1, res2 = aver(lst1), aver(lst2)
+# print("Avarage of List_1:", res1)
+# print("Avarage of List_2:", res2)
+if res1 > res2:
+    print('The average of the "List_1" more than the average of the "List_2"')
+if res2 > res1:
+    print('The average of the "List_2" more than the average of the "List_1"')
+if res1 == res2:
+    print('Average values of two lists are equal')
+
+# 27. Создайте список из 11 случайных целых чисел из отрезка [-1;1], выведите список на экран в
+# строку. Определите какой элемент встречается в списке чаще всего и выведите об этом
+# сообщение на экран. Если два каких-то элемента встречаются одинаковое количество раз,
+# то не выводить ничего.
+
+def fill_list(list):
+    lst = [random.randint(-1, 1) for i in range(11)]
+    return lst
+
+lst = fill_list(list)
+print(lst)
+
+def most_freq_elem(list):
+    elem = lst[0]
+    max_freq = 1
+    for i in range(len(lst) - 1):
+        freq = 1
+        for n in range(i + 1, len(lst)):
+            if lst[i] == lst[n]:
+                freq += 1
+        if freq > max_freq:
+            max_freq = freq
+            elem = lst[i]
+    if max_freq > 1:
+        return elem
+
+result = most_freq_elem(lst)
+print("The number '%d' is the most common" % (result))
