e8539629ef6a8270cd2298c542b1464944557e26
Oleg <o.v.gamov@gmail.com>
2016-12-08 21:15:49 +0200

    remade

--- #33.py
+++ #33.py
@@ -1,41 +1,38 @@
 # 33. Создать два класса: Окружность и Точка. Создать в классе окружности метод,
 # который принимает в качестве параметра точку и проверяет находится ли данная точка внутри окружности.
 
-class Parameters(object):
+class Shape(object):
 
-    def __init__(self, absc, ord):
-        self.absc = absc
-        self.ord = ord
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
+class Point(Shape):
 
-class Point(Parameters):
+    def __init__(self, x, y):
+        Shape.__init__(self, x, y)
 
-    def __init__(self, absc, ord):
-        Parameters.__init__(self, absc, ord)
+class Circle(Shape):
 
+    def __init__(self, x, y, r):
+        Shape.__init__(self, x, y)
+        self.r = r
 
-class Circle(Parameters):
-
-    def __init__(self, absc, ord, rad):
-        Parameters.__init__(self, absc, ord)
-        self.rad = rad
-
-    def check_point(self):
+    def check_point(self, point):
         import math
-        if (math.sqrt((pnt.absc - crcl.absc) ** 2 + (pnt.ord - crcl.ord) ** 2)) <= crcl.rad:
+        if (math.sqrt((point.x - self.x) ** 2 + (point.y - self.y) ** 2)) <= self.r:
             print("\t""Yes. The point is included in the circle""\n")
         else:
             print("\t""No. The point is not included in the circle""\n")
 
-
-crcl = Circle(2, 1, 1)
-print("Center coordinates (%g, %g) of the circlec with radius = %g" % (crcl.absc, crcl.ord, crcl.rad))
+crcl = Circle(2, 1, 5)
+print("Center coordinates (%g, %g) of the circlec with radius = %g" % (crcl.x, crcl.y, crcl.r))
 pnt = Point(3, 3)
-print("Point coordinates is (%g, %g)" % (pnt.absc, pnt.ord))
-crcl.check_point()
+print("Point coordinates is (%g, %g)" % (pnt.x, pnt.y))
+crcl.check_point(pnt)
 
-crcl = Circle(1, 3, 2.5)
-print("Center coordinates (%g, %g) of the circlec with radius = %g" % (crcl.absc, crcl.ord, crcl.rad))
-pnt = Point(1, 2)
-print("Point coordinates is (%g, %g)" % (pnt.absc, pnt.ord))
-crcl.check_point()
+crc2 = Circle(1, 3, 2.5)
+print("Center coordinates (%g, %g) of the circlec with radius = %g" % (crc2.x, crc2.y, crc2.r))
+pnt2 = Point(1, 2)
+print("Point coordinates is (%g, %g)" % (pnt.x, pnt.y))
+crc2.check_point(pnt2)
